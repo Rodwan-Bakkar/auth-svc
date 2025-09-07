c4127a114c61b56bf05d7ab29948e254fecdf16a
Rodwan-Bakkar <rodwan.bakkar@gmail.com>
2025-09-07 17:08:17 +0200

    first commit

---
+++ src/main/kotlin/com/auth/AuthController.kt
@@ -0,0 +1,11 @@
+package com.auth
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class AuthController {
+
+    @GetMapping
+    fun helloworld() = "Hello World"
+}
